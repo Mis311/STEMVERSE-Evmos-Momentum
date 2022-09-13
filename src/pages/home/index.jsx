8d1f9212b10f14c3c875ac2cc2f3e391f3202646
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-09-13 19:53:58 +0500

    Adding Components

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,20 @@ import { Link } from "react-router-dom";
 import Button from "../../components/button/button";
 import Layout from "../../components/layouts/single-sided";
 
-function Section_1() {
+function Title(props) {
   return (
-    <div className="home__section_1 row space-between">
+    <div className={"title " + props.className}>
+      <h1>{props.content || "Stemverse Land"}</h1>
+      <hr />
+    </div>
+  );
+}
+function Home_Index() {
+  return (
+    <div className="home__section_1 row space-between min-screen-height">
       {/* Title 1 */}
-      <div>
-        <h1 className="home__title row ">STEM</h1>
+      <div className="home__title">
+        <h1>STEM</h1>
         <div className="row space-between">
           <Button caps={1} content="Connect" className="primary-color" />
           <Button caps={1} content="Info" className="primary-color" />
@@ -16,8 +24,7 @@ function Section_1() {
 
       {/* Title 2 */}
       <div>
-        <h1 className="home__title primary-color">VERSE</h1>
-        <hr />
+        <Title content="Verse" className="home__title primary-color" />
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt
@@ -27,34 +34,97 @@ function Section_1() {
   );
 }
 
-function Section_2() {
+function Home_About() {
   return (
-    <div className="min-screen-height">
-      {/* <Title content="About" /> */}
+    <div className="home__section_2 min-screen-height">
+      <h2>About</h2>
+      <hr />
+      {/* Container */}
+      <div className="container">
+        {/*  */}
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </div>
+      </div>
+
       {/* Container */}
       {/* <Number content='01'/> */}
     </div>
   );
 }
 
-function Section_3() {
-  return <div className="min-screen-height"></div>;
+function Card(props) {
+  <div className="card">
+    <div className="card__image"></div>
+  </div>;
 }
-function Section_4() {
-  return <div className="min-screen-height"></div>;
+function Home_Story() {
+  return (
+    <div className="home__section_3 min-screen-height">
+      <h2>Story</h2>
+      <hr />
+      <div>
+        <div className="home__card">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          sit provident dolore nulla impedit voluptatem illo facere officiis,
+          veritatis quidem earum atque ea temporibus optio molestiae nihil
+          quibusdam doloribus eaque.
+          <div className="card__style_1"></div>
+          {/* 
+          <Card />
+          <Card />
+          <Card />
+          <Card /> 
+          */}
+        </div>
+      </div>
+    </div>
+  );
 }
-function Section_5() {
-  return <div className="min-screen-height"></div>;
+
+function CardContianer(props) {
+  return (
+    <div>
+      <div>
+        <h2>{props.name || "Learn as You Earn"}</h2>
+        <p>
+          {props.description ||
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aperiam voluptate voluptates."}
+        </p>
+      </div>
+      <Card />
+    </div>
+  );
+}
+
+function Home_Game_And_Features() {
+  return (
+    <div className="home__section_4 min-screen-height">
+      <h2>Games And Features</h2>
+      <hr />
+
+      {/* Containers */}
+      <CardContianer />
+    </div>
+  );
+}
+function Newsletter() {
+  return <div className="home__section_5 min-screen-height"></div>;
 }
 
 export default function Home(props) {
   return (
     <Layout>
-      <Section_1 />
-      <Section_2 />
-      <Section_3 />
-      <Section_4 />
-      <Section_5 />
+      <Home_Index />
+      <Home_About />
+      <Home_Story />
+      <Home_Game_And_Features />
+      <Newsletter />
     </Layout>
   );
 }
