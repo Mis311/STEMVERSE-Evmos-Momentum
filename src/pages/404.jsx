71ccef814327c0c21bd03a0c6648a71439a3a37e
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-09-15 17:43:15 +0500

    Creating 404 Page

---
+++ src/pages/404.jsx
@@ -0,0 +1,20 @@
+export default () => {
+  return (
+    <div className="error">
+      <div>
+        <span className="abstract-boxes"></span>
+        <h1>404</h1>
+        <span className="abstract-boxes"></span>
+      </div>
+      <p>
+        You just landed in a restricted zone that is yet to be discovered, for
+        your security, we created this back button to teleport you to a safe
+        zone.{" "}
+        <span className="primary-color">
+          <em> Enabling Secure Root and Readying Systems </em>
+        </span>
+      </p>
+      <button className="btn-2"></button>
+    </div>
+  );
+};
--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,4 +1,32 @@
 // Profile
 export default (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <h1></h1>
+      <div className="user-profile">
+        <div className="user-profile-img"></div>
+        <div className="user-profile-container">
+          {/* Name */}
+          <h2>{props.name || "Ley Hanamura"}</h2>
+
+          {/* Info */}
+          <div className="row">
+            <span className="user-tag">{props.title || "UI Designer"}</span>
+            <span className="user-tag-fol">
+              Following • {props.following || "11"}
+            </span>
+            <span className="user-tag-fol">
+              Followers • {props.followers || "132k"}
+            </span>
+          </div>
+
+          {/* Description */}
+          <div>
+            {props.description ||
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
