83191e81bd1c0811baec95117a6fdf4f0d6ef104
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-09-15 18:22:38 +0500

    Adding images and random generated data

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,59 @@ import Profile from "./pages/profile/profile";
 import Settings from "./pages/profile/settings";
 import GhostNFTSlot from "./pages/support/ghostnft-slot";
 
+// Generated Data
+let userData = {
+    name: "Ley Hanamura",
+    title: "UI Designer",
+    following: "11",
+    followers: "132k",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    imageURL: "assets/users/ley.png",
+  },
+  friendsData = [
+    // Online
+    {
+      name: "Violet Sparks",
+      imageURL: "assets/users/friends/user-1.png",
+      status: "online",
+    },
+    {
+      name: "Johnny Pena",
+      imageURL: "assets/users/friends/user-2.png",
+      status: "online",
+    },
+    {
+      name: "Horace Mccarthy",
+      imageURL: "assets/users/friends/user-1.png",
+      status: "online",
+    },
+
+    // Offline
+    {
+      name: "Karen May",
+      imageURL: "assets/users/friends/user-2.png",
+      status: "offline",
+    },
+    {
+      name: "Percy Hunter",
+      imageURL: "assets/users/friends/user-1.png",
+      status: "offline",
+    },
+
+    // Pending
+    {
+      name: "Rodolfo Lawson",
+      imageURL: "assets/users/friends/user-2.png",
+      status: "pending",
+    },
+    {
+      name: "Anne Barnett",
+      imageURL: "assets/users/friends/user-1.png",
+      status: "pending",
+    },
+  ];
+
 export default function App() {
   return (
     <div>
@@ -19,7 +72,10 @@ export default function App() {
         <Route path="/ghost-nft-slot" element={<GhostNFTSlot />} />
 
         {/* User Related */}
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/profile"
+          element={<Profile {...userData} {...friendsData} />}
+        />
         <Route path="/settings" element={<Settings />} />
 
         {/* Error Rendering */}
