a6e1f123cb68ed16fa3cdeddfd0e048907deaf8a
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-09-15 19:29:29 +0500

    Wallet Conenct HTML completed

---
+++ src/components/checks/wallet-connection.jsx
@@ -0,0 +1,14 @@
+export default function WalletConnection() {
+  return (
+    <div className="card">
+      <h1>
+        <span className="primary">Connect</span> Wallet
+      </h1>
+      <p>
+        To proceed with the GhostNFT Slot, please make sure to connect your
+        wallet so a succesful transaction can be made while using the slot
+      </p>
+      <button className="btn-1">Connect</button>
+    </div>
+  );
+}
