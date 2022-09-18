14dd7ada2d67859efd7c808b2408e53d48f7818b
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-09-18 20:15:08 +0500

    Fixing Back Button

--- src/components/header-footer/header.jsx
+++ src/components/header-footer/header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Button from "../button/button";
+import { useState } from "react";
 
 function UserCard(props) {
   return (
@@ -39,180 +40,203 @@ function UserCard(props) {
     </div>
   );
 }
-
-export default function Header(props) {
+function AddFriends() {
   return (
-    <header
-      className={
-        "row hr-center vr-center space-between " + (props.user && "signedin")
-      }
-    >
-      <div className="row priority-4">
-        {/* Logo */}
-        <Link to="/" className="flex hr-center vr-center">
-          <img className="logo" src="assets/logo.svg" alt="logo" />
-        </Link>
+    <div className="header__add_friends_container row hr-center vr-center min-screen-height full-width">
+      <div>Hello</div>
+    </div>
+  );
+}
+export default function Header(props) {
+  const [addFriends, setAddFriends] = useState(false);
+  return (
+    <>
+      {/* Header */}
+      <header
+        className={
+          "row hr-center vr-center space-between " + (props.user && "signedin")
+        }
+      >
+        {/* Navigation Items */}
+        <div className="row priority-4">
+          {/* Logo */}
+          <Link to="/" className="flex hr-center vr-center">
+            <img className="logo" src="assets/logo.svg" alt="logo" />
+          </Link>
 
-        {/* Content */}
-        <nav className="flex hr-center">
-          <a href="#" className="language-options">
-            En
-          </a>
-          <ul className="no-bullets row space-evenly hr-center no-padding">
-            <a href="#about">
-              <li>About</li>
+          {/* Content */}
+          <nav className="flex hr-center">
+            <a href="#" className="language-options">
+              En
             </a>
-            <a href="#story">
-              <li>Story</li>
-            </a>
-            <a href="#games-and-features">
-              <li>Game & Features</li>
-            </a>
-            <Link to="/ghost-nft-shell">
-              <li>Ghost NFT Slot</li>
-            </Link>
-            <Link to="/stemverse-land">
-              <li>Stemverse Land</li>
-            </Link>
-          </ul>
-        </nav>
-      </div>
+            <ul className="no-bullets row space-evenly hr-center no-padding">
+              <a href="#about">
+                <li>About</li>
+              </a>
+              <a href="#story">
+                <li>Story</li>
+              </a>
+              <a href="#games-and-features">
+                <li>Game & Features</li>
+              </a>
+              <Link to="/ghost-nft-shell">
+                <li>Ghost NFT Slot</li>
+              </Link>
+              <Link to="/stemverse-land">
+                <li>Stemverse Land</li>
+              </Link>
+            </ul>
+          </nav>
+        </div>
 
-      {/* Connect */}
-      <div className={props.navigation.sidebar && "active"}>
-        {props.user ? (
-          <>
-            {/* Backgrounds */}
-            <div className="header__navigation_hover priority-4">
-              <div>
-                <UserCard
-                  user={true}
-                  status="online"
-                  className="header__loggedin_user"
-                  copy={true}
-                />
-                <h3 className="uppercase">Social</h3>
-                {/* Pending */}
+        {/* Connected User + Friends */}
+        <div className={props.navigation.sidebar && "active"}>
+          {props.user ? (
+            <>
+              {/* Backgrounds */}
+              <div className="header__navigation_hover priority-4">
                 <div>
-                  <h4 className="uppercase">Pending • 3</h4>
-                  <div>
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                      copy={true}
-                      cancel={true}
-                    />
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                      copy={true}
-                      cancel={true}
-                    />
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                      copy={true}
-                      cancel={true}
-                    />
-                  </div>
-                </div>
+                  {/* User */}
+                  <UserCard
+                    user={true}
+                    status="online"
+                    className="header__loggedin_user"
+                    copy={true}
+                  />
+                  <h3 className="uppercase">Social</h3>
 
-                {/* Online */}
-                <div>
-                  <h4 className="uppercase">Online • 3</h4>
+                  {/* Pending */}
                   <div>
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                    />
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                    />
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                    />
+                    <h4 className="uppercase">Pending • 3</h4>
+                    <div>
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                        copy={true}
+                        cancel={true}
+                      />
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                        copy={true}
+                        cancel={true}
+                      />
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                        copy={true}
+                        cancel={true}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                {/* Offline */}
-                <div>
-                  <h4 className="uppercase">Offline • 3</h4>
+                  {/* Online */}
                   <div>
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                    />
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                    />
-                    <UserCard
-                      data="0x0190fqw12r12 ... e591"
-                      className="header__user"
-                    />
+                    <h4 className="uppercase">Online • 3</h4>
+                    <div>
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                      />
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                      />
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                      />
+                    </div>
                   </div>
-                </div>
 
-                {/* Add Friends */}
-                <div
-                  class="header__add_friends row hr-center"
-                  onClick={() => {
-                    console.log("Add friends div");
-                  }}
-                >
-                  {/* margin-top: 20px; */}
-                  <div class="row hr-center vr-center">
-                    <span>+</span>
+                  {/* Offline */}
+                  <div>
+                    <h4 className="uppercase">Offline • 3</h4>
+                    <div>
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                      />
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                      />
+                      <UserCard
+                        data="0x0190fqw12r12 ... e591"
+                        className="header__user"
+                      />
+                    </div>
                   </div>
-                  <p>Add Friends</p>
+
+                  {/* Add Friends */}
+                  <div
+                    class="header__add_friends row hr-center"
+                    onClick={() => {
+                      setAddFriends(true);
+                    }}
+                  >
+                    {/* margin-top: 20px; */}
+                    <div class="row hr-center vr-center">
+                      <span>+</span>
+                    </div>
+                    <p>Add Friends</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div
-              className={"header__navigation_hover2"}
-              onClick={() => {
-                props.setNavigation(false);
-              }}
-            ></div>
+              <div
+                className={"header__navigation_hover2"}
+                onClick={() => {
+                  props.setNavigation(false);
+                }}
+              ></div>
 
-            {/* Header Profile */}
-            <div
-              className={"header__user-profile"}
-              onClick={() => {
-                props.setNavigation(true);
-              }}
-            ></div>
+              {/* Header Profile */}
+              <div
+                className={"header__user-profile"}
+                onClick={() => {
+                  props.setNavigation(true);
+                }}
+              ></div>
 
-            {/* Friends */}
-            <div className="header__user-friends priority-5">
-              <div className="header__user-profile"></div>
-              <div className="header__user-profile"></div>
-              <div className="header__user-profile"></div>
-              <div className="header__user-profile"></div>
-            </div>
+              {/* Friends */}
+              <div
+                className="header__user-friends priority-5"
+                onClick={() => {
+                  props.setNavigation(true);
+                }}
+              >
+                <div className="header__user-profile"></div>
+                <div className="header__user-profile"></div>
+                <div className="header__user-profile"></div>
+                <div className="header__user-profile"></div>
+              </div>
 
-            {/* Back button */}
-            <div
-              className="header__back_button"
+              {/* Back button */}
+              <div
+                className="header__back_button row hr-center vr-center"
+                onClick={() => {
+                  props.setNavigation(false);
+                }}
+              >
+                <span className="row hr-center vr-center">→</span>
+              </div>
+            </>
+          ) : (
+            <Button
+              className="px-5-radius"
+              content="Connect Wallet"
               onClick={() => {
-                props.setNavigation(false);
+                props.connectWallet();
+                console.log(props.account);
               }}
-            ></div>
-          </>
-        ) : (
-          <Button
-            className="px-5-radius"
-            content="Connect Wallet"
-            onClick={() => {
-              props.connectWallet();
-              console.log(props.account);
-            }}
-          />
-        )}
-      </div>
-    </header>
+            />
+          )}
+        </div>
+      </header>
+
+      {/* Add Friends */}
+      {addFriends && <AddFriends />}
+    </>
   );
 }
 
