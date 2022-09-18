5c68cf93987ac08b60105d62a9b3665f28d63d9c
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-09-19 01:18:42 +0500

    Updating 404 looks

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,25 +1,29 @@
 import { Link } from "react-router-dom";
+import Layout from "../components/layouts/single-sided";
+
 export default function Error404() {
   return (
-    <div className="error min-screen-height column hr-center vr-center">
-      <div>
-        <span className="error__abstract-box_1"></span>
-        <h1>404</h1>
-        <span className="error__abstract-box_2"></span>
+    <Layout>
+      <div className="error min-screen-height column hr-center vr-center">
+        <div>
+          <span className="error__abstract-box_1"></span>
+          <h1>404</h1>
+          <span className="error__abstract-box_2"></span>
+        </div>
+        <p>
+          You just landed in a restricted zone that is yet to be discovered, for
+          your security, we created this back button to teleport you to a safe
+          zone.{" "}
+          <span className="primary-color">
+            <strong>
+              <em> Enabling Secure Root and Readying Systems </em>
+            </strong>
+          </span>
+        </p>
+        <Link to="/">
+          <button className="btn-2">Head Back</button>
+        </Link>
       </div>
-      <p>
-        You just landed in a restricted zone that is yet to be discovered, for
-        your security, we created this back button to teleport you to a safe
-        zone.{" "}
-        <span className="primary-color">
-          <strong>
-            <em> Enabling Secure Root and Readying Systems </em>
-          </strong>
-        </span>
-      </p>
-      <Link to="/">
-        <button className="btn-2">Head Back</button>
-      </Link>
-    </div>
+    </Layout>
   );
 }
