b21f6a687ba8eeba938ae33adf6ac5c8637aaab6
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-09-20 22:49:49 +0500

    Adding images and updating content

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,6 @@
 import Button from "../../components/button/button";
 import Layout from "../../components/layouts/single-sided";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function Title(props) {
   if (props.center)
@@ -17,11 +17,6 @@ function Title(props) {
       {props.content}
     </h2>
   );
-
-  // <div className={"title " + props.className}>
-  //   <h2>{props.content || "Stemverse Land"}</h2>
-  //   <hr />
-  // </div>
 }
 
 function Home_Index() {
@@ -44,7 +39,9 @@ function Home_Index() {
             className="uppercase primary-color no-margin"
           />
           <p>
-            Connect with STEM (Science, Technilogy, Engineering &nbsp; Mathmatics) learners and workers to revive the lost Mega STEM World City, a.k.a STEMVERSE.
+            Connect with STEM (Science, Technilogy, Engineering &nbsp;
+            Mathmatics) learners and workers to revive the lost Mega STEM World
+            City, a.k.a STEMVERSE.
           </p>
         </div>
       </div>
@@ -63,20 +60,35 @@ function Home_About() {
         <div className="home__card">
           <div>
             <p>
-              STEMVERSE is another dimention of our world.	<br />
-              In another world, technology advanced faster than us and got ruined by some "human-related disaster". The case files and hints are scattered in the ruin of STEMVERSE.
+              STEMVERSE is another dimention of our world. <br />
+              In another world, technology advanced faster than us and got
+              ruined by some "human-related disaster". The case files and hints
+              are scattered in the ruin of STEMVERSE.
             </p>
 
             <p>
-              As a new STEMVERSE resident, your goal is to match up with other users and complete daily tasks, including your homeworks and project as well as collaborative online projects as a programmer and scientist.
+              As a new STEMVERSE resident, your goal is to match up with other
+              users and complete daily tasks, including your homeworks and
+              project as well as collaborative online projects as a programmer
+              and scientist.
             </p>
 
             <p>
-              You as a carrier of STEM, STEMVERSE and our world gets closer and coldsleep characters will be able to revive and walk on earth again as more world NFTs you will collect and solve mysteries.
+              You as a carrier of STEM, STEMVERSE and our world gets closer and
+              coldsleep characters will be able to revive and walk on earth
+              again as more world NFTs you will collect and solve mysteries.
             </p>
 
             <p>
-              In the torn world of STEMVERSE, the story continues in two ways: past and present. In the past, there was disaster that led to STEMVERSE. In the STEMVERSE World, you can meet the past residents, now in coldsleep state, through exploring our metaverse and also by doing a slot. In order to earn tokens you can login daily, take online courses, complete STEM related tasks and socialize. Ghosts will drop some hints and keywords what happened, and in present it is up to your contribution to revive the lost world, and make it even more alive.
+              In the torn world of STEMVERSE, the story continues in two ways:
+              past and present. In the past, there was disaster that led to
+              STEMVERSE. In the STEMVERSE World, you can meet the past
+              residents, now in coldsleep state, through exploring our metaverse
+              and also by doing a slot. In order to earn tokens you can login
+              daily, take online courses, complete STEM related tasks and
+              socialize. Ghosts will drop some hints and keywords what happened,
+              and in present it is up to your contribution to revive the lost
+              world, and make it even more alive.
             </p>
           </div>
         </div>
@@ -98,44 +110,62 @@ function Card(props) {
 
 let stories = [
   {
-    category: 'Science Empire',
-    description: 'The home for all science learners, from biochemistry to neuroscience, either interest or job, comes together and learn/work together.',
-    image: 'https://images.indianexpress.com/2022/07/midjourney-featured.png'
+    category: "Science Empire",
+    description:
+      "The home for all science learners, from biochemistry to neuroscience, either interest or job, comes together and learn/work together.",
+    image: "backgrounds/home/science-empire.webp",
   },
   {
-    name: 'Technology Nation',
-     description: 'Programmers and data scientists come home, mingle and gingle and develop ',
-    image: 'https://images.indianexpress.com/2022/07/monsoon-midjourney-2.png?resize=600,600'
+    name: "Technology Nation",
+    description:
+      "Programmers and data scientists come home, mingle and gingle and develop ",
+    image: "backgrounds/home/technology-nation.webp",
   },
   {
-    name: 'Engineering Union',
-    description: 'Engineers of universities, professionals, or DIY engineers belong here to study, work and earn.',
-    image: 'https://i.pinimg.com/originals/c6/bd/06/c6bd06d037065db6c5c59d618d1d1753.png'
+    name: "Engineering Union",
+    description:
+      "Engineers of universities, professionals, or DIY engineers belong here to study, work and earn.",
+    image: "backgrounds/home/engineering-union.webp",
   },
-    {
-    name: 'Mathematics Federation',
-    description: 'From the statistics to heavy carriculation, anyone fascinated by numbers, we have your home nest consisted from 0 and 1',
-    image: 'https://cdna.artstation.com/p/assets/images/images/050/301/526/large/teo-mathlein-c306a643-640e-43ea-b2d8-81bf3a20ec56-teomath-httpss-mj-runajnasu-kowloon-walled-city-slums-unreal-engine-environment-concept-art-trending-on-art.jpg?1654539468'
+  {
+    name: "Mathematics Federation",
+    description:
+      "From the statistics to heavy carriculation, anyone fascinated by numbers, we have your home nest consisted from 0 and 1",
+    image: "backgrounds/home/mathematics-federation.webp",
   },
-  
-]
+];
 
 function StoryCard(props) {
+  const [onStoryCard, setOnStoryCard] = useState(true);
+  function setCard(bool) {
+    setOnStoryCard(bool);
+  }
   return (
     <div
       className={"card__style_1 " + props.className}
+      onClick={() => {
+        setOnStoryCard(false);
+      }}
       style={{
-        background: `url(${props.image})`,
+        background: `linear-gradient(rgba(3, 31, 49, 0.7), rgba(3, 31, 49, 0.7)), url('assets/${props.image}')`,
+        backgroundSize: "cover",
       }}
     >
-      <OnStoryCard {...props} />
+      <OnStoryCard
+        description={props.description}
+        name={props.name}
+        image={props.image}
+        className={"" + (onStoryCard ? "hidden" : "active")}
+        setCard={setCard}
+      />
     </div>
   );
 }
 
 function OnStoryCard(props) {
   return (
-    <div>
+    <div className={"" + props.className}>
+      {console.log(props)}
       {/* Lesser z-index and has an overlay */}
       <div
         className="on_story_card_bg"
@@ -147,19 +177,32 @@ function OnStoryCard(props) {
         }}
       ></div>
 
-      {/* Cross */}
-      <div className="full-width row flex-end">X</div>
+      {/*  */}
+      <div className="flex hr-center vr-center min-screen-height">
+        <div className="story__card_active">
+          {/* Cross */}
+          <div
+            className="full-width row vr-end"
+            onClick={() => {
+              props.setCard(true);
+            }}
+          >
+            X
+          </div>
 
-      {/* Scrollable Data */}
-      <div className={"card__style_1 " + props.className}>
-        <h2>
-          {/* <span>World: </span> {props.title || "Newl Astorpic"} */}<span>World: STEM</span> {props.category || "STEMVERSE"}	
-        </h2>
-        <p>
-          {props.description}{/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. */}
-        </p>
-        <img src="" alt="" />
+          {/* Scrollable Data */}
+          <div className={"card__style_2 " + props.className}>
+            <h2>
+              <span>World: STEM</span> {props.name || "STEMVERSE"}
+            </h2>
+            <p>{props.description}</p>
+            <img
+              src={"assets/" + props.image}
+              alt={props.name}
+              className="full-width"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -171,26 +214,43 @@ function Home_Story() {
       <Title content="Story" />
       <div>
         <div>
-          Once upon a time, humanity has reached its highest form, the greatest realm, and the city had the significant 4 divisions:<em>Science, Tech, Engineering, and Math</em>. <br />Now at the same place, the regions are governed by a surviving DAO. All that remain are philosophers' dreams.
+          <p>
+            Once upon a time, humanity has reached its highest form, the
+            greatest realm, and the city had the significant 4 divisions:
+            <em> Science, Tech, Engineering, and Math</em>.
+          </p>
+          <p>
+            Now at the same place, the regions are governed by a surviving DAO.
+            All that remain are philosophers' dreams.
+          </p>
+
           <div className="flex flex-wrap vr-center hs3_card_container">
             {/* Section 1 */}
-            <div className="row">
-              {stories.map((story, index) => {
-            return (
-          <StoryCard
-            key={index}
-            category={story.category}
-            description={story.description}
-            image={story.image}
+            <div className="row flex-wrap">
+              <StoryCard
+                name={stories[0].name}
+                description={stories[0].description}
+                image={stories[0].image}
               />
-  )}
-)}
+              <StoryCard
+                name={stories[1].name}
+                description={stories[1].description}
+                image={stories[1].image}
+              />
             </div>
 
             {/* Section 2 */}
-            <div className="row">
-              <StoryCard />
-              <StoryCard />
+            <div className="row flex-wrap">
+              <StoryCard
+                name={stories[2].name}
+                description={stories[2].description}
+                image={stories[2].image}
+              />
+              <StoryCard
+                name={stories[3].name}
+                description={stories[3].description}
+                image={stories[3].image}
+              />
             </div>
           </div>
         </div>
